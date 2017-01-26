554115f6143587c987f3ebe277dc4f4bf6998f67
ZENG-LAPTOP\EvEr <zengxianhan93@gmail.com>
2017-01-26 10:25:49 +0800

    Delegate layout finish

--- app/src/main/java/com/adprojectmobile/activity/department/DelegateAuthority/DelegateAuthority.java
+++ app/src/main/java/com/adprojectmobile/activity/department/DelegateAuthority/DelegateAuthority.java
@@ -1,9 +1,14 @@
 package com.adprojectmobile.activity.department.DelegateAuthority;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.adprojectmobile.R;
+import com.adprojectmobile.model.Employee;
 
 public class DelegateAuthority extends AppCompatActivity {
 
@@ -11,5 +16,35 @@ public class DelegateAuthority extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.delegate_authority_activity_delegate_authority);
+
+        final Employee employee=getIntent().getParcelableExtra("data");
+
+        EditText editTextName=(EditText)findViewById(R.id.editText_delegate_employeeName);
+        EditText editTextStatus=(EditText)findViewById(R.id.editText_delegate_status);
+        final EditText editTextStartDate=(EditText)findViewById(R.id.editText_delegate_startDate);
+        final EditText editTextEndDate=(EditText)findViewById(R.id.editText_delegate_enDate);
+        Button btnConfirmDelegate=(Button) findViewById(R.id.btn_delegate_confirm);
+
+        editTextName.setText(employee.getName());
+        if(!employee.isDelegated()){
+            editTextStatus.setText("UnAuthorized");
+        }
+        else {
+            editTextStatus.setText("Authorized");
+        }
+
+        btnConfirmDelegate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String startDate=editTextStartDate.getText().toString();
+                String endDate=editTextEndDate.getText().toString();
+
+                Intent intent=new Intent(getApplicationContext(),RevokeAuthority.class);
+                intent.putExtra("data",employee);
+                intent.putExtra("start",startDate);
+                intent.putExtra("end",endDate);
+                startActivity(intent);
+            }
+        });
     }
 }
